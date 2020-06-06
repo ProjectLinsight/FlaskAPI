a597647a30bdfe952a80a6d6c5db17e994df1be8
dilky97 <2017cs039@stu.ucsc.cmb.ac.lk>
2020-06-06 19:58:32 +0530

    api test results out

--- app.py
+++ app.py
@@ -5,5 +5,52 @@ import os
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:linsight37@localhost/linsight' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init DB
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+#Results Class
+class Results(db.Model):
+    __tablename__ ='results'
+    id = db.Column(db.BigInteger, primary_key=True)
+    index = db.Column(db.String(255))
+    grade = db.Column(db.String(255))
+    subjectCode = db.Column(db.String(255))
+    batch = db.Column(db.String(255))
+    yoe = db.Column(db.String(255))
+    semester = db.Column(db.String(255))
+    created_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp())
+    updated_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp())
+
+    def __init__(self,index,grade,subjectCode,batch,yoe,semester,created_at,updated_at):
+        self.index = index
+        self.grade = grade
+        self.subjectCode = subjectCode
+        self.batch = batch
+        self.yoe = yoe
+        self.semester = semester
+        self.created_at = created_at
+        self.updated_at = updated_at
+    
+class ResultsSchema(ma.Schema):
+    class Meta:
+        fields = ('id','index','grade','subjectCode','batch','yoe','semester','created_at','updated_at')
+
+#Init Schema
+result_schema = ResultsSchema(strict=True)
+results_schema = ResultsSchema(many=True,strict=True)
+
+#Routes
+@app.route('/results',methods =['GET'])
+def get_results():
+    all_results = Results.query.all()
+    query_out = results_schema.dump(all_results)
+    return jsonify(query_out.data)
+
 if __name__ == '__main__':
     app.run(debug=True)
